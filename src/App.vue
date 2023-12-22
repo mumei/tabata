cfc08b718d201289335a3f6beaf3584f186ed0a8
yuto <yuto.uehara@mail.rooxim.com>
2023-12-22 23:28:38 +0900

    release

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import headerArea from './components/header.vue'
 import aboutArea from './components/about.vue'
-import Menu from "./components/menu.vue";
-import Footer from "./components/footer.vue";
+import Menu from './components/menu.vue';
+import Footer from './components/footer.vue';
 import Shop from './components/shop.vue';
-import Access from "./components/access.vue";
+import Access from './components/access.vue';
+import ActionMenu from './components/ActionMenu.vue'
 </script>
 <template>
   <div class="root">
@@ -14,6 +15,7 @@ import Access from "./components/access.vue";
     <Shop />
     <Access />
     <Footer />
+    <action-menu />
   </div>
 </template>
 
