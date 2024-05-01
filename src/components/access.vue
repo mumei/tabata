2dd3d2563edc30fcf839884271af23f8f7aef0e9
yuto <yuto.uehara@mail.rooxim.com>
2024-05-01 20:12:36 +0900

    update for seo.

--- src/components/access.vue
+++ src/components/access.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div id="access">
-    <div class="title">アクセス</div>
+    <h1 class="title">アクセス</h1>
     <div class="main">
       <div>
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d25692.629175870334!2d139.102689!3d36.395193!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601ef2360fb0589b%3A0x8ebb5a04f9658b2e!2z5omL5L2c44KK55Sw55WR44Gu54S844GN44G-44KT44GY44KF44GG!5e0!3m2!1sen!2sus!4v1703113674802!5m2!1sen!2sus" width="100%" height="450" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
